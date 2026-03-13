b1b4b0f422fde12ce7d1aa10adb560c97991743d
OsmarG <240036@utxicotepec.edu.mx>
2026-03-13 09:49:17 -0600

    crear tabla de usuario

---
+++ migrations/20260313151604-create-usuario.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false
+       
+        
+      },
+      nombre: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      direccion: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+      },
+      telefono: {
+        type: Sequelize.STRING(15),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      rol: {
+        type: Sequelize.ENUM('cliente', 'admin'),
+        allowNull: false,
+        defaultValue: 'cliente'
+      },
+      fecha_registro: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
---
+++ models/tbc_usuarios.js
@@ -0,0 +1,56 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    static associate(models) {
+      // relaciones aquí
+    }
+  }
+
+  Usuario.init({
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+
+    direccion: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+
+    telefono: {
+      type: DataTypes.STRING(15),
+      allowNull: false
+    },
+
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+
+    rol: {
+      type: DataTypes.ENUM('cliente', 'admin'),
+      allowNull: false,
+      defaultValue: 'cliente'
+    },
+
+    fecha_registro: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+    tableName: 'tbc_usuarios',
+    timestamps: false
+  });
+
+  return Usuario;
+};
